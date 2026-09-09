0cfec67eac64b48cadb0e07ae5fdf83e215105f5
Hyacenthe-k <karindahyacenthe1@gmail.com>
2026-09-09 12:46:05 -0700

    Add console.py command interpreter and complete models

--- console.py
+++ console.py
@@ -1,32 +1,149 @@
 #!/usr/bin/python3
-"""Defines the entry point of the command interpreter for the AirBnB
-clone project.
-
-This module implements HBNBCommand, a class based on Python's cmd
-module, which provides the interactive (and non-interactive) shell
-used to create, inspect, and manage the project's models.
-"""
+"""Defines the HBNBCommand console."""
 import cmd
+import shlex
+from models import storage
+from models.base_model import BaseModel
+from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
 
 
 class HBNBCommand(cmd.Cmd):
-    """Command interpreter for the AirBnB clone project."""
+    """Command interpreter for HBNB."""
 
     prompt = "(hbnb) "
+    __classes = {
+        "BaseModel",
+        "User",
+        "State",
+        "City",
+        "Amenity",
+        "Place",
+        "Review"
+    }
 
-    def emptyline(self):
-        """Do nothing when an empty line is entered."""
-        return False
-
-    def do_quit(self, line):
+    def do_quit(self, arg):
         """Quit command to exit the program."""
         return True
 
-    def do_EOF(self, line):
-        """EOF signal (Ctrl+D) to exit the program."""
+    def do_EOF(self, arg):
+        """EOF signal to exit the program."""
         print("")
         return True
 
+    def emptyline(self):
+        """Do nothing on empty line + ENTER."""
+        pass
 
-if __name__ == "__main__":
+    def do_create(self, arg):
+        """Creates a new instance of BaseModel, saves it, and prints the id."""
+        args = shlex.split(arg)
+        if len(args) == 0:
+            print("** class name missing **")
+        elif args[0] not in HBNBCommand.__classes:
+            print("** class doesn't exist **")
+        else:
+            new_instance = eval(args[0])()
+            new_instance.save()
+            print(new_instance.id)
+
+    def do_show(self, arg):
+        """Prints string representation of an instance based on class name and id."""
+        args = shlex.split(arg)
+        objdict = storage.all()
+        if len(args) == 0:
+            print("** class name missing **")
+        elif args[0] not in HBNBCommand.__classes:
+            print("** class doesn't exist **")
+        elif len(args) == 1:
+            print("** instance id missing **")
+        elif "{}.{}".format(args[0], args[1]) not in objdict:
+            print("** no instance found **")
+        else:
+            print(objdict["{}.{}".format(args[0], args[1])])
+
+    def do_destroy(self, arg):
+        """Deletes an instance based on class name and id."""
+        args = shlex.split(arg)
+        objdict = storage.all()
+        if len(args) == 0:
+            print("** class name missing **")
+        elif args[0] not in HBNBCommand.__classes:
+            print("** class doesn't exist **")
+        elif len(args) == 1:
+            print("** instance id missing **")
+        elif "{}.{}".format(args[0], args[1]) not in objdict:
+            print("** no instance found **")
+        else:
+            del objdict["{}.{}".format(args[0], args[1])]
+            storage.save()
+
+    def do_all(self, arg):
+        """Prints string representation of all instances based or not on class name."""
+        args = shlex.split(arg)
+        if len(args) > 0 and args[0] not in HBNBCommand.__classes:
+            print("** class doesn't exist **")
+        else:
+            objlist = []
+            for obj in storage.all().values():
+                if len(args) > 0 and args[0] == obj.__class__.__name__:
+                    objlist.append(obj.__str__())
+                elif len(args) == 0:
+                    objlist.append(obj.__str__())
+            print(objlist)
+
+    def do_update(self, arg):
+        """Updates an instance based on class name and id by adding/updating attribute."""
+        args = shlex.split(arg)
+        objdict = storage.all()
+        if len(args) == 0:
+            print("** class name missing **")
+            return
+        if args[0] not in HBNBCommand.__classes:
+            print("** class doesn't exist **")
+            return
+        if len(args) == 1:
+            print("** instance id missing **")
+            return
+        if "{}.{}".format(args[0], args[1]) not in objdict:
+            print("** no instance found **")
+            return
+        if len(args) == 2:
+            print("** attribute name missing **")
+            return
+        if len(args) == 3:
+            print("** value missing **")
+            return
+
+        obj = objdict["{}.{}".format(args[0], args[1])]
+        attr_name = args[2]
+        attr_value = args[3]
+
+        if attr_name in ["id", "created_at", "updated_at"]:
+            return
+
+        if hasattr(obj, attr_name):
+            attr_type = type(getattr(obj, attr_name))
+            try:
+                attr_value = attr_type(attr_value)
+            except (ValueError, TypeError):
+                pass
+        else:
+            if attr_value.isdigit():
+                attr_value = int(attr_value)
+            else:
+                try:
+                    attr_value = float(attr_value)
+                except ValueError:
+                    pass
+
+        setattr(obj, attr_name, attr_value)
+        storage.save()
+
+
+if __name__ == '__main__':
     HBNBCommand().cmdloop()
